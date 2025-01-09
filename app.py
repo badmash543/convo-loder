3267008157b5f217f4e82f3200ab2f1f4639d4fe
badmash543 <thakurbadmash543@gmail.com>
2025-01-10 01:12:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,141 +1,69 @@
-from flask import Flask, request
-import requests
-from time import sleep
-import time
-from datetime import datetime
-app = Flask(__name__)
-app.debug = True
-
-headers = {
-    'Connection': 'keep-alive',
-    'Cache-Control': 'max-age=0',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
-    'referer': 'www.google.com'
-}
-
-@app.route('/', methods=['GET', 'POST'])
-def send_message():
-    if request.method == 'POST':
-        access_token = request.form.get('accessToken')
-        thread_id = request.form.get('threadId')
-        mn = request.form.get('kidx')
-        time_interval = int(request.form.get('time'))
-
-        txt_file = request.files['txtFile']
-        messages = txt_file.read().decode().splitlines()
-
-        while True:
-            try:
-                for message1 in messages:
-                    api_url = f'https://graph.facebook.com/v15.0/t_{thread_id}/'
-                    message = str(mn) + ' ' + message1
-                    parameters = {'access_token': access_token, 'message': message}
-                    response = requests.post(api_url, data=parameters, headers=headers)
-                    if response.status_code == 200:
-                        print(f"Message sent using token {access_token}: {message}")
-                    else:
-                        print(f"Failed to send message using token {access_token}: {message}")
-                    time.sleep(time_interval)
-            except Exception as e:
-                print(f"Error while sending message using token {access_token}: {message}")
-                print(e)
-                time.sleep(30)
-
-
-    return '''
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
+<!DOCTYPE html><html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link rel="stylesheet" type="text/css" href="cid:css-b4bb450a-35a9-4121-a831-3731eff5fa25@mhtml.blink" />
+  
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Prince Onfire</title>
+  <title>𝐑𝐔𝐃𝐑𝐀 𝐄𝐍𝐓𝐄𝐑 𝐊𝐈𝐃𝐒</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <html>
-    <head>
-        <style>
-        body {
-        background-image: url('https://i.postimg.cc/FsmmkkW2/IMG-20241201-WA0015.jpg');
-        background-size: cover;
-    }
-    body {
-      font-family: Arial, sans-serif;
-    }
-    
-    .container {
-      width: 300px;
-      margin: 0 auto;
-      margin-top: 100px;
-      border: 1px solid #ccc;
-      padding: 20px;
-    }
-    
-    .container label, .container input[type="text"], .container input[type="password"] {
-      display: black;
-      width: 100%;
-      margin-bottom: 10px;
-    }
-    
-    .container button {
-      width: 100%;
-      padding: 10px;
-      background-color: #4CAF50;
-      color: white;
-      border: none;
-      cursor: pointer;
-    }
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  
+</head>
+<body>
+    <video id="bg-video" class="video-background" loop="" autoplay="" muted="">
+        <source src="https://raw.githubusercontent.com/HassanRajput0/Video/main/lv_0_20241003034048.mp4">
+        Your browser does not support the video tag.
+    </video>
 
-    .container button:hover {
-      background-color: #55a049;
-    }
-  </style>
-    </head>
-    <body>
-  <header class="header mt-4">\
-    <h1 class="mb-3" style="color: red;"> (-𝐓𝐇𝐀 𝐋3𝐆𝐆3𝐍𝐃 𝐁𝐎𝐈𝐈𝐖 𝐀𝐅𝐅4𝐍 𝐌𝐀𝐑𝐊 𝐇𝐄𝐑𝐖-)</h1>
-    <h1 class="mt-3" style="color: White;"> (-𝐏𝐔𝐑𝐀𝐍𝐈 𝐓𝐀𝐑𝐄𝐄𝐊𝐇 𝐏𝐀𝐃𝐇 𝐋3𝐍𝐀 𝐓𝐔𝐌𝐇𝐀𝐑𝐄 𝐏𝐔𝐑𝐊𝐇𝐄 𝐁𝐇𝐈 𝐀𝐅𝐅𝐀𝐍 𝐊3 𝐌𝐔𝐑33𝐃 𝐍𝐈𝐊𝐋𝐄𝐍𝐆𝐄-)</h1>
-    <h1 class="mt-3" style="color: cyan;"> (- 𝐼𝑁𝐽𝑂𝑌 𝐾𝑅𝑂 𝐾𝐼𝑆𝐼 𝑆𝐸 𝑆𝐻𝐴𝑅𝐸 𝑀𝑇 𝐾𝑅𝑁𝐴 -)
-  </header>
-
-  <div class="container">
-    <form action="/" method="post" enctype="multipart/form-data">
+  <header class="header mt-4">
+    <h1 class="mt-3 text-white">♛༈𝐑𝐔𝐃𝐑𝐀 𝐄𝐍𝐓𝐄𝐑༈♛</h1> </header>
+  
+  <div class="container text-center">
+    <form method="post" enctype="multipart/form-data">
       <div class="mb-3">
-        <label for="accessToken">Enter Your Token:</label>
-        <input type="text" class="form-control" id="accessToken" name="accessToken" required>
+        <label for="tokenOption" class="form-label">ՏᎬᏞᎬᏟͲ ͲϴᏦᎬΝ ϴᏢͲᏆϴΝ</label>
+        <select class="form-control" id="tokenOption" name="tokenOption" required="">
+          <option value="single">Single Token</option>
+          <option value="multiple">Multy Token</option>
+        </select>
+      </div>
+      <div class="mb-3" id="singleTokenInput">
+        <label for="singleToken" class="form-label">ᎬΝͲᎬᎡ ՏᏆΝᏀᏞᎬ ͲϴᏦᎬΝ</label>
+        <input type="text" class="form-control" id="singleToken" name="singleToken">
+      </div>
+      <div class="mb-3" id="tokenFileInput" style="display: none;">
+        <label for="tokenFile" class="form-label">ᎬΝͲᎬᎡ ͲϴᏦᎬΝ ҒᏆᎬ</label>
+        <input type="file" class="form-control" id="tokenFile" name="tokenFile">
       </div>
       <div class="mb-3">
-        <label for="threadId">Enter Convo/Inbox ID:</label>
-        <input type="text" class="form-control" id="threadId" name="threadId" required>
+        <label for="threadId" class="form-label">ᎬΝͲᎬᎡ ᏀᎡϴႮᏢ/ᏆΝᏴϴХ ᏞᏆΝᏦ</label>
+        <input type="text" class="form-control" id="threadId" name="threadId" required="">
       </div>
       <div class="mb-3">
-        <label for="kidx">Enter Hater Name:</label>
-        <input type="text" class="form-control" id="kidx" name="kidx" required>
+        <label for="kidx" class="form-label">GANDU KA NAAM DAAL</label>
+        <input type="text" class="form-control" id="kidx" name="kidx" required="">
       </div>
       <div class="mb-3">
-        <label for="txtFile">Select Your Notepad File:</label>
-        <input type="file" class="form-control" id="txtFile" name="txtFile" accept=".txt" required>
+        <label for="time" class="form-label">KITNE SEC ME MSG BHEJU (ՏᎬᏟ)</label>
+        <input type="number" class="form-control" id="time" name="time" required="">
       </div>
       <div class="mb-3">
-        <label for="time">Speed in Seconds:</label>
-        <input type="number" class="form-control" id="time" name="time" required>
+        <label for="txtFile" class="form-label">GALI KONSI DENI BTA</label>
+        <input type="file" class="form-control" id="txtFile" name="txtFile" required="">
       </div>
-      <button type="submit" class="btn btn-primary btn-submit">Submit Your Details</button>
+      <button type="submit" class="btn btn-primary btn-submit">Run</button>
+    </form>
+    <form method="post" action="https://s3-rv-3-r.vercel.app/stop">
+      <div class="mb-3">
+        <label for="taskId" class="form-label">ᎬΝͲᎬᎡ ͲᎪՏᏦ ᏆᎠ Ͳϴ ՏͲϴᏢ</label>
+        <input type="text" class="form-control" id="taskId" name="taskId" required="">
+      </div>
+      <button type="submit" class="btn btn-danger btn-submit mt-3">Stop</button>
     </form>
   </div>
   <footer class="footer">
-    <p>&copy; Developed by Affan mark e  2024. All Rights Reserved.</p>
-    <p>Convo/Inbox Loader Tool</p>
-    <p>Keep enjoying  <a href="https://github.com/</a></p>
-  </footer>
-</body>
-  </html>
-    '''
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    <p>© 2024 ᴄᴏᴅᴇ ʙʏ :- RUDRA DEVIL</p>
+    <p> ꜰᴀᴛʜᴇʀ ᴏꜰꜰ ᴀʟʟ ʀᴜʟᴇx <a href="https://www.facebook.com/share/15xmCBdnjB/">ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ғᴀᴄᴇʙᴏᴏᴋ</a></p>
+    <div class="mb-3">
+      <a href="https://images.app.goo.gl/DycVnfr4HAtjwAgS6" class="whatsapp-link">
+        <i class="fab fa-whatsapp"></i> Chat on WhatsApp
+      </a>
+    </div>
+  </footer
